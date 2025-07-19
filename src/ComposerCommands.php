8d37afa4e0f0d2a8ed21bd3496f8bacb52015265
Austin Passy <367897+thefrosty@users.noreply.github.com>
2025-07-19 09:54:29 -0700

    Code cleanup, removing Steam back to Buffer.
    - New Search Command method.
    - Better filtering of flags.

--- src/ComposerCommands.php
+++ src/ComposerCommands.php
@@ -8,11 +8,12 @@ use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Output\BufferedOutput;
 use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\Console\Output\OutputInterface;
+use TheFrosty\WpComposer\Contracts\Commands;
 use function array_filter;
 use function array_map;
 use function array_merge;
-use function count;
 use function explode;
+use function in_array;
 use function sanitize_text_field;
 use function sprintf;
 
@@ -65,9 +66,29 @@ trait ComposerCommands
         return $output;
     }
 
+    public function search(string $args, string $flags): OutputInterface
+    {
+        if (empty($args)) {
+            (new ConsoleOutput())->writeln('Error: Missing required argument');
+        }
+        $this->getComposer()->run(
+            new ArrayInput(
+                array_merge(
+                    ['command' => __FUNCTION__],
+                    $this->getFlags($flags, [Commands::ARG_ONLY_NAME, Commands::ARG_ONLY_VENDOR])
+                ),
+            ),
+            $output = new BufferedOutput()
+        );
+        return $output;
+    }
+
     public function diagnose(): OutputInterface
     {
-        $this->getComposer()->run(new ArrayInput(['command' => 'diagnose', '--no-ansi' => true]), $output = new BufferedOutput());
+        $this->getComposer()->run(
+            new ArrayInput(['command' => __FUNCTION__, Commands::ARG_NO_ANSI => true]),
+            $output = new BufferedOutput()
+        );
         return $output;
     }
 
@@ -75,19 +96,25 @@ trait ComposerCommands
     {
         $output = new BufferedOutput();
         $output->writeln(sprintf('WpComposer version %s', esc_attr(WpComposer::VERSION)));
-        $this->getComposer()->run(new ArrayInput(['-V' => true, '--no-ansi' => true]), $output);
+        $this->getComposer()->run(
+            new ArrayInput([Commands::ARG_VERSION => true, Commands::ARG_NO_ANSI => true]),
+            $output
+        );
         return $output;
-    }
-
-    protected function getFlags(string $flags): array
-    {
-        $_flags = array_map(static fn($str): string => sanitize_text_field($str), explode(',', $flags));
-
-        return array_fill_keys($_flags, true);
     }
 
     protected function getComposer(): WpComposer
     {
         return $this->composer;
+    }
+
+    protected function getFlags(string $flags, ?array $allowed = null): array
+    {
+        $_flags = array_map(static fn($str): string => sanitize_text_field($str), explode(',', $flags));
+        if ($allowed !== null) {
+            $_flags = array_filter($_flags, static fn($flag): bool => in_array($flag, $allowed, true));
+        }
+
+        return array_fill_keys($_flags, true);
     }
 }
